aad51220cfe9506f8e042ca1a32de435b2bb51ec
GorantlaAkhil <89672154+GorantlaAkhil@users.noreply.github.com>
2022-09-02 16:28:06 -0400

    adda

---
+++ Hi.java
@@ -0,0 +1,13 @@
+package com.example.reactive_java;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class example {
+    public static void main(String[] args) {
+       New File
+
+    }
+}
---
+++ hello.java
@@ -0,0 +1,19 @@
+package com.example.reactive_java;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class example {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+        map.put("key1","value1");
+        map.put("key2","value2");
+        map.put("key3","value3");
+
+        List<String> list = new ArrayList<>();
+       //map.stream
+
+    }
+}
